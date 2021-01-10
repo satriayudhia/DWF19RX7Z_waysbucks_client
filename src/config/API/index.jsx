d1fb10e0beb9e8fced3bdf08d0a5175bd757642a
satriayudhiap <satria.yudhiap@gmail.com>
2021-01-10 16:03:14 +0700

    deploy

--- src/config/API/index.jsx
+++ src/config/API/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: 'http://dwf19rx7z-waysbucks.herokuapp.com/api/v1'
+    baseURL: 'https://dwf19rx7z-waysbucks.herokuapp.com/api/v1'
 })
 
 export const setAuthToken = (token) => {
